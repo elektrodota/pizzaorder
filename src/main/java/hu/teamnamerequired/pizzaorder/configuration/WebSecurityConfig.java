36a71fadf11c5d5aac50c6617b8813cad256837f
roli666 <aszalsr@gmail.com>
2018-10-22 14:05:49 +0200

    security config

--- src/main/java/hu/teamnamerequired/pizzaorder/configuration/WebSecurityConfig.java
+++ src/main/java/hu/teamnamerequired/pizzaorder/configuration/WebSecurityConfig.java
@@ -2,12 +2,14 @@ package hu.teamnamerequired.pizzaorder.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -15,32 +17,30 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+          .withUser("admin").password(passwordEncoder().encode("admin")).roles("USER");
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests()
-                .antMatchers("/", "/home").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .formLogin()
-                .loginPage("/Login")
-                .permitAll()
-                .and()
-                .logout()
-                .permitAll();
+          .authorizeRequests()
+          .antMatchers("/Login*").anonymous()
+          .anyRequest().authenticated()
+          .and()
+          .formLogin()
+          .loginPage("/Login.html")
+          .defaultSuccessUrl("/Home.html")
+          .failureUrl("/Login.html?error=true")
+          .and()
+          .logout().logoutSuccessUrl("/Login.html");
     }
-
+ 
     @Bean
-    @Override
-    public UserDetailsService userDetailsService() {
-
-        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-        UserDetails user =
-                User.withUsername("admin")
-                        .password(encoder.encode("admin"))
-                        .roles("USER")
-                        .build();
-
-        return new InMemoryUserDetailsManager(user);
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
