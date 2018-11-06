153dbb498e0110dce8ef00f69c308ab6cbed7cc0
roli666 <aszalsr@gmail.com>
2018-11-06 18:40:46 +0100

    i18n-t próbálgattam

--- src/main/java/hu/teamnamerequired/pizzaorder/controllers/HomeController.java
+++ src/main/java/hu/teamnamerequired/pizzaorder/controllers/HomeController.java
@@ -23,4 +23,8 @@ public class HomeController {
         return model;
 
     }
+    @GetMapping("/international")
+    public String getInternationalPage() {
+        return "international";
+    }
 }
