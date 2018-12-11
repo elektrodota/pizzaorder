b7145ec6b1031c41741b5940047169beeefdd81f
elektrodota <krisz962010@hotmail.com>
2018-12-11 17:48:44 +0100

    getPizza get request

--- src/main/java/hu/teamnamerequired/pizzaorder/controllers/ShoppingCartController.java
+++ src/main/java/hu/teamnamerequired/pizzaorder/controllers/ShoppingCartController.java
@@ -4,6 +4,7 @@ package hu.teamnamerequired.pizzaorder.controllers;
 import hu.teamnamerequired.pizzaorder.dtos.PizzaDto;
 import hu.teamnamerequired.pizzaorder.interfaces.PizzaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.http.MediaType;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -11,12 +12,15 @@ import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpServletRequest;
 
 @RestController
+@EnableAutoConfiguration
 public class ShoppingCartController {
     @Autowired
     PizzaService pizzaService;
 
-    @PostMapping(value = "/shoppingCartController/api/getPizza", produces = MediaType.APPLICATION_JSON_VALUE)
-    public PizzaDto getPizza(@RequestBody long id,HttpServletRequest httpServletRequest,Model uiModel) {
+    @GetMapping(value = "/shoppingCartController/api/getPizza", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public PizzaDto getPizza(@RequestParam long id,HttpServletRequest httpServletRequest,Model uiModel) {
+        System.out.println("itt vagyok");
         PizzaDto result = new PizzaDto(pizzaService.getPizzaById(id));
         return result;
     }
--- src/main/resources/content/scripts/main.js
+++ src/main/resources/content/scripts/main.js
@@ -1,14 +1,14 @@
 function setCookie(cname, cvalue, extradays) {
     var d = new Date();
     d.setTime(d.getTime() + (extradays * 24 * 60 * 60 * 1000));
-    var expires = "expires="+d.toUTCString();
+    var expires = "expires=" + d.toUTCString();
     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
 
 function getCookie(cname) {
     var name = cname + "=";
     var ca = document.cookie.split(';');
-    for(var i = 0; i < ca.length; i++) {
+    for (var i = 0; i < ca.length; i++) {
         var c = ca[i];
         while (c.charAt(0) == ' ') {
             c = c.substring(1);
@@ -19,69 +19,67 @@ function getCookie(cname) {
     }
     return "";
 }
+
 function getPizza(id) {
-    console.log("id:",id);
+    console.log("id:", id);
     $.ajax({
-        type : "POST",
-        contentType : "application/json",
-        url : "/shoppingCartController/api/getPizza",
-        data : id,
-        success : function(data) {
+        type: "GET",
+        contentType: "application/json",
+        url: "/shoppingCartController/api/getPizza",
+        data: id,
+        success: function (data) {
             console.log("SUCCESS: ", data);
-            return data;
+            $("#shoppingCartItems").append(
+                `
+  <li id="${data.pizzaID}">
+    <img src="${data.pizzaImage}"/>${data.pizzaName}<a class="removeFromCart" href="#"></a>
+  </li>`);
         },
-        error : function(e) {
+        error: function (e) {
             console.log("ERROR: ", e);
         },
-        done : function(e) {
+        done: function (e) {
             console.log("DONE");
         }
     });
 }
+
 //Document ready function
-$(function(){
-var items = [];
-if(getCookie(items)!="")
-{
-    items=getCookie(items);
-}
-var priceTotal;
+$(function () {
+    var items = [];
+    if (getCookie(items) != "") {
+        items = getCookie(items);
+    }
+    var priceTotal;
 
 // Add Item to Cart
-$('.addToCart').click(function (event){
-    event.preventDefault();
-  let id = $(this.closest(".card")).attr("data-id");
-  console.log("id:",id);
-  let size = $(this.closest(".card")).find("select").val();
-  items.push({pizzaID:id,pizzaSize:size});
-  setCookie("items",items,1);
-
-  let pizza = getPizza(id);
-
-  $("#shoppingCartItems").append(function (pizza) {
-      `
-  <li id="${pizza.pizzaID}">
-    <img src="${pizza.pizzaImage}"/>${pizza.pizzaName}<a class="removeFromCart" href="#"></a>
-  </li>`});
+    $('.addToCart').click(function (event) {
+        event.preventDefault();
+        let id = $(this.closest(".card")).attr("data-id");
+        console.log("id:", id);
+        let size = $(this.closest(".card")).find("select").val();
+        items.push({pizzaID: id, pizzaSize: size});
+        setCookie(items, items, 1);
 
 
-  $('#itemCount').text(items.length).css('display', 'block');
-  price=0;
-  priceTotal += price;
-});
+
+        $('#itemCount').text(items.length).css('display', 'block');
+        price = 0;
+        priceTotal += price;
+    });
 
 // Remove Item From Cart
-$('#shoppingCartModal').on('click', '.removeFromCart', function(){
+    $('#shoppingCartModal').on('click', '.removeFromCart', function () {
 
-  $('#itemCount').text(items.length);
+        $('#itemCount').text(items.length);
 
-  // Remove Cost of Deleted Item from Total Price
-  var price = parseInt($(this).siblings().find('.price').text());
-  priceTotal -= price;
+        // Remove Cost of Deleted Item from Total Price
+        var price = parseInt($(this).siblings().find('.price').text());
+        priceTotal -= price;
 
-  if (itemCount == 0) {
-    $('#itemCount').css('display', 'none');
-  }
-});
+        if (itemCount == 0) {
+            $('#itemCount').css('display', 'none');
+        }
+    });
 
 });
